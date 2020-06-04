584c0889735af028705263397f3f78fb8b0ee8b0
Bob Paauwe <bpaauwe@yahoo.com>
2020-06-04 13:59:17 -0700

    Add zone commands.
    These are the commands that the russound can send to the ISY by
    pressing buttons on the keypad device.

--- nodes/zone.py
+++ nodes/zone.py
@@ -46,6 +46,15 @@ class Zone(polyinterface.Node):
         Add zone command
     '''
 
+    '''
+    Called when the zone's keypad is used.  Send the keypress to the ISY
+    '''
+    def keypress(self, key):
+        LOGGER.info('Sending ' + key + ' to ISY')
+        # is this something the controller class has but the node class
+        # doesn't? How can a node send a command?
+        self.reportCmd(key, 0)
+
     def set_power(self, power):
         self.setDriver('ST', power, True, True, 2)
 
@@ -63,3 +72,25 @@ class Zone(polyinterface.Node):
 
     def set_balance(self, vol):
         self.setDriver('GV4', vol, True, True, 56)
+
+    def process_cmd(self, cmd=None):
+        LOGGER.info('ISY sent: ' + str(cmd))
+
+    commands = {
+            'HOME': process_cmd,
+            'REV': process_cmd,
+            'FWD': process_cmd,
+            'PLAY': process_cmd,
+            'SELECT': process_cmd,
+            'LEFT': process_cmd,
+            'RIGHT': process_cmd,
+            'DOWN': process_cmd,
+            'UP': process_cmd,
+            'BACK': process_cmd,
+            'REPLAY': process_cmd,
+            'INFO': process_cmd,
+            'BACKSPACE': process_cmd,
+            'SEARCH': process_cmd,
+            'ENTER': process_cmd,
+            }
+
