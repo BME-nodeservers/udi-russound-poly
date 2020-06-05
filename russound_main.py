1eba34f41f5c77998ec96e225d18525bedf4a0ab
Bob Paauwe <bpaauwe@yahoo.com>
2020-06-05 15:37:45 -0700

    Add set keypad message and get zone info
    Add functions to send commands to the controller to set all keypads
    with a message and get all zone info from a zone.

--- russound_main.py
+++ russound_main.py
@@ -106,6 +106,7 @@ def russound_loop_udp(sock, processCommand):
                     if b == 0xf7:
                         buf[st] = b
                         st = 0
+                        _LOGGER.warning('recv: ' + ' '.join('{:02x}'.format(x) for x in data))
                         msg = rnet_message.RNetMessage(buf)
                         processCommand(msg)
                     else:
@@ -118,6 +119,108 @@ def russound_loop_udp(sock, processCommand):
         except ConnectionResetError as msg:
             _LOGGER.error('Connection error: ' + msg)
             russound_connected = False
+
+def setIDs(data, start, control_id, zone_id, keypad_id):
+    data[start]     = control_id
+    data[start + 1] = zone_id
+    data[start + 2] = keypad_id
+
+def setPath(data, start, target, source= []):
+    idx = start
+
+    data[idx] = len(target)
+    idx += 1
+    for i in range(0, len(target)):
+        data[idx] = target[i]
+        idx += 1
+
+    data[idx] = len(source)
+    for i in range(0, len(source)):
+        data[idx] = source[i]
+        idx += 1
+
+
+def checksum(data, length):
+    cksum = 0
+
+    for b in data:
+        cksum += b
+
+    cksum += length
+    cksum = cksum & 0x007f
+
+    return cksum
+
+# Send a request to the controller to send various types of information
+# about a specific zone.
+#  0x0401 - current volume
+#  0x0402 - selected source
+#  0x0406 - current on/off state
+#  0x0407 - all info
+#  0x0500 - current bass level
+#  0x0501 - current treble level
+#  0x0502 - current loudness level
+#  0x0503 - current balance
+#  0x0404 - current turn on volume
+#  0x0505 - current background color
+#  0x0506 - current do not distrub
+#  0x0507 - current party mode
+def russound_get_info(sock, zone, info_type):
+    _LOGGER.warning('Entered get_info()')
+    path_len = (info_type & 0xff00) >> 8
+    if path_len == 5:
+        data = bytearray(18)
+    else:
+        data = bytearray(17)
+
+    data[0] = 0xf0
+    setIDs(data, 1, 0, 0, 0x7f)
+    setIDs(data, 4, 0, zone, 0x70)
+    data[7] = 0x01
+
+    if path_len == 5:
+        setPath(data, 8, [0x02, 0x00, zone, 0x00, (info_type & 0x00ff)])
+        data[15] = 0x00
+        data[16] = checksum(data, 16)
+        data[17] = 0xf7
+    else:
+        setPath(data, 8, [0x02, 0x00, zone, (info_type & 0x00ff)])
+        data[14] = 0x00
+        data[15] = checksum(data, 15)
+        data[16] = 0xf7
+
+    _LOGGER.warning('sending: ' + ''.join('{:02x}'.format(x) for x in data))
+    sock.sendto(data, ('192.168.92.38', 5000))
+
+def russound_send_msg(sock, zone):
+    data = bytearray(36)
+    data[0] = 0xf0
+    setIDs(data, 4, 0, 0, 0x70)
+    data[7] = 0
+    data[21] = ord('a')
+    data[22] = ord(' ')
+    data[23] = ord('m')
+    data[24] = ord('e')
+    data[25] = ord('s')
+    data[26] = ord('s')
+    data[27] = ord('a')
+    data[28] = ord('g')
+    data[29] = ord('e')
+    setIDs(data, 1, 0x7f, 0, 0)
+    setPath(data, 8, [0x01, 0x01], [])
+    data[12] = 0x00
+    data[13] = 0x00
+    data[14] = 0x01
+    data[15] = 0x00
+    data[16] = 0x10
+    data[17] = 0x00
+
+    data[34] = checksum(data, 34)
+    data[35] = 0xf7
+
+    _LOGGER.warning('sending: ' + ' '.join('{:02x}'.format(x) for x in data))
+    sock.sendto(data, ('192.168.92.38', 5000))
+
 
 
 def russound_loop_udp_old(sock, processCommand):
