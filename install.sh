527f4965c541805f677a4aad7808bed5079717a6
Bob Paauwe <bpaauwe@yahoo.com>
2020-06-09 09:24:58 -0700

    Fix type and requirements list

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ pip3 install -r requirements.txt --user
 
 if [ ! -f profile/nls/en_us.txt ]
 then
-	cp template/nsl/en_us.txt profile/nls/en_us.txt
+	cp template/nls/en_us.txt profile/nls/en_us.txt
 fi
