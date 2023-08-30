6a79c382c88291a92a4eca6476bfe906b57782a3
Bob Paauwe <bpaauwe@yahoo.com>
2023-08-30 11:58:24 -0700

    Version 2.1.1

--- russound.py
+++ russound.py
@@ -15,7 +15,7 @@ LOGGER = udi_interface.LOGGER
 if __name__ == "__main__":
     try:
         polyglot = udi_interface.Interface([russound.RSController, zone.Zone])
-        polyglot.start('2.1.0')
+        polyglot.start('2.1.1')
         control.Controller(polyglot)
         polyglot.runForever()
     except (KeyboardInterrupt, SystemExit):
