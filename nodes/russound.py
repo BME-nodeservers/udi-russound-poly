a38d3f6515767ba17606d0c3f3a28acc3dd0df15
Bob Paauwe <bpaauwe@yahoo.com>
2024-02-24 10:24:57 -0800

    Remove Drop() test.
    The test was successful in the sense that it does drop the
    connection.  But even doing this doesn't seem to reproduce what
    ever issue UDI thought was caused by the node server.

--- nodes/russound.py
+++ nodes/russound.py
@@ -133,7 +133,6 @@ class RSController(udi_interface.Node):
             #  for RNET we have to call request_config with the controller numbers and see what happens?
             #   can we make use of the queue here?
             if self.rnet.protocol == 'RNET':
-                self.rnet.Drop()
                 for ctrl in range(1, 6):
                     # Get zone/source configuration for controller 1
                     self.poly.Notices['init'] = 'Requesting configuration for controller {}'.format(ctrl)
@@ -175,6 +174,7 @@ class RSController(udi_interface.Node):
         if not self.rnet.connected:
             self.setDriver("ST", 0)
             # Connection has failed.
+            self.rnet.Drop()
 
     def query(self):
         self.reportDrivers()
